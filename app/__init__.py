0987c95d3ff48b00b8fe4554f08da01cff7b5c93
agarwalp10 <agarwal.p10@gmail.com>
2025-11-05 14:17:03 -0500

    Updated code

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,20 @@ from app.blueprints.customers import customers_bp
 from app.blueprints.mechanics import mechanics_bp
 from app.blueprints.service_tickets import service_tickets_bp
 from app.blueprints.parts import parts_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+# these define the URL paths for SWAGGER UI and API's OpenAPI spec
+SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml' # Our API url (can of course be a local resource)
+
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Mechanic Shop API"
+    },
+)
 
 #this is our application factory
 def create_app(config_name):
@@ -29,5 +43,7 @@ def create_app(config_name):
     app.register_blueprint(service_tickets_bp, url_prefix='/service_tickets')
     # inventory_bp is imported from app/blueprints/inventory/__init__.py
     app.register_blueprint(parts_bp, url_prefix='/parts')
+    # register swagger blueprint
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) 
 
     return app
